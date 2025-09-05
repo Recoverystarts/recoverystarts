91e4b314757a723cec95f39bf599aaadac450acf
Recoverystarts <dmccorriston2222@proton.me>
2025-09-05 13:12:31 -0700

    Add catch-all route to serve index.html for unknown routes

--- server.js
+++ server.js
@@ -44,6 +44,12 @@ app.use(express.static(path.join(__dirname, "public")));
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
+app.get("*", (req, res) => {
+  // If request looks like a file (has a dot), return 404
+  if (req.path.includes(".")) return res.status(404).send("Not Found");
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
 
 // --- Chat endpoint ---
 app.post("/api/chat", async (req, res) => {
